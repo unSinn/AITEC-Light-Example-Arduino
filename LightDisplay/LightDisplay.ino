3bc266ea5ee5a2978d8025d829aa25c67fc9321e
unSinn <fabian.maechler@gmail.com>
2013-10-21 11:05:42 +0200

    Minor code formatting for presentation

--- LightDisplay/LightDisplay.ino
+++ LightDisplay/LightDisplay.ino
@@ -1,16 +1,18 @@
 #include "TM1637.h"
+
 #define CLK 2 // Port D2 on Grove Shield
 #define DIO 3
 
 #define LIGHT_PIN A0
 
-TM1637 tm1637(CLK,DIO);
+TM1637 tm1637 = TM1637(CLK,DIO);
 
 void setup()
 {
   tm1637.init();
   tm1637.set(BRIGHT_TYPICAL);//BRIGHT_TYPICAL = 2,BRIGHT_DARKEST = 0,BRIGHTEST = 7;
 }
+
 void loop()
 {
   delay(150);
